34fce784dbee0344c26ac836488ead34e03986c1
zdzd3643 <zdzd3643@gmail.com>
2023-09-18 13:02:31 +0900

    ff

--- 001.HTML학습/06.CGV_PJ/003.구현소스/js/main.js
+++ 001.HTML학습/06.CGV_PJ/003.구현소스/js/main.js
@@ -63,3 +63,30 @@ pmenu.forEach(ele=>{
         ele.parentElement.classList.add('on');
     });
 }); //////////// forEach ////////////
+
+////////////// 맵버튼 클릭시 맵박스 보이기 ///////////////////
+// 대상: 이벤트 - .btn-map, 변경 - .gmap
+// 변경내용: 맵버튼 클릭시 .gmap에 클래스 on넣기
+//          닫기버튼 클릭시 .gmap에 클래스 on 빼기
+// 1. 대상선정
+const btnMap = domFn.qs('.btn-map');
+const gmap = domFn.qs('.gmap');
+const cbtn = domFn.qs('.cbtn');
+console.log('대상:',btnMap,gmap,cbtn);
+
+// 2. 이벤트설정 및 함수구현
+// 2-1. 맵버튼 클릭시
+domFn.addEvt(btnMap,'click',()=>{
+    // 구글맵보이기
+    gmap.classList.add('on');
+    // 가림막보이기
+    document.body.classList.add('on');
+}); ////////// click 이벤트 함수 /////////////
+
+// 2-2. 닫기버튼 클릭시
+domFn.addEvt(cbtn,'click',()=>{
+    // 구글맵숨기기
+    gmap.classList.remove('on');
+    // 가림막숨기기
+    document.body.classList.remove('on');
+}); ////////// click 이벤트 함수
