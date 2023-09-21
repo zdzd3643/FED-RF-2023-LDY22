091046c93a33b4249800333cb644e8162c19d96f
zdzd3643 <zdzd3643@gmail.com>
2023-09-21 18:24:09 +0900

    ff

--- 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
@@ -6,7 +6,7 @@ import dFn from "./dom.js";
 // 부드러운 스크롤 모듈
 import { startSS, setPos } from "./smoothScroll23.js";
 // 데이터모듈
-import { gridData, gnbData } from "./data_drama.js";
+import { gridData, gnbData, previewData } from "./data_drama.js";
 
 // 부드러운 스크롤 적용 /////////////////
 startSS();
@@ -37,10 +37,10 @@ const gridBox = dFn.qsa(".grid-box");
 console.log("대상:", gridBox);
 
 // 2. 대상 코드넣기 함수 호출설정하기 /////////
-gridBox.forEach((ele,idx)=>makeGrid(ele,idx));
+gridBox.forEach((ele, idx) => makeGrid(ele, idx));
 
 // 3. 그리드 스타일 데이터 생성하기 함수
-function makeGrid(ele,idx) { 
+function makeGrid(ele, idx) {
     // ele - 대상요소 / idx - 순번(데이터순번)
     // 1. 현장포토 데이터를 기반으로 HTML코드 만들기
     let hcode = "<ul>";
@@ -50,12 +50,14 @@ function makeGrid(ele,idx) {
 
     gridData[idx].forEach((val) => {
         // html변수에 계속 넣기
-        // 폴더경로는 idx가 0이면'live_photo' 
+        // 폴더경로는 idx가 0이면'live_photo'
         // 1이면'poster_img'로 셋팅함!
         hcode += `
                     <li>
                         <figure>
-                            <img src="images/${idx?'poster_img':'live_photo'}/${val.imgName}.jpg" alt="${val.title}">
+                            <img src="images/${
+                                idx ? "poster_img" : "live_photo"
+                            }/${val.imgName}.jpg" alt="${val.title}">
                             <figcaption>${val.title}</figcaption>
                         </figure>
                     </li>
@@ -150,3 +152,68 @@ function outFn() {
     // 서브메뉴 박스 높이값 0만들기!
     dFn.qsEl(this, ".smenu").style.height = "0px";
 } //////////// outFn 함수 ////////////
+
+//////////////////////////////////////////////////
+//// 인트로 동영상 클릭시 플레이하기 ///////////
+// 대상: .intro.mv.img
+// 이벤트: click
+// -> 가상요소 플레이버튼 클릭시
+// 이벤트 버블링으로 본 박스가 반응함!
+
+// 1. 대상선정
+const mvBox = dFn.qs(".intro-mv-img");
+
+// 2. 이벤트 설정하기
+dFn.addEvt(mvBox, "click", showMv);
+
+// 이벤트연결 상태변수
+let stsShowMv = 0;
+
+// 3. 함수만들기
+function showMv() {
+    if (stsShowMv) return;
+    stsShowMv = 1; // 한번만 실행
+
+    console.log("보여줘!");
+    // 동영상 넣기
+    // 대상: 나자신('.intro-mv-img')
+    this.innerHTML = `
+        <video src='./images/intro_mv.mp4' autoplay controls></video>
+    `;
+
+    // 가상요소 플레이버튼 없애기위해 .off 지우기
+    this.classList.remove("off");
+} //////////// showMv 함수 ////////////
+
+//////////////////////////////////////////
+// 오름차순 데이터를 내림차순으로 변경하여 화면에 뿌리기!
+
+// 1. 데이터 정렬 변경하기
+let preNewData = previewData.sort((x, y) => {
+    // x,y는 배열값 앞뒤를 계속 가지고 들어옴
+    // 배열값 중 idx속성값을 가져와서 숫자형 변환 후 사용
+    let a = Number(x.idx);
+    let b = Number(y.idx);
+
+    // 배열 순서변경 메서드인 sort() 내부에 return값을
+    // 사용하여 순서를 변경한 새로운 배열을 만들어준다!
+    return (a = b ? 0 : a > b ? -1 : 1);
+    // 비?집:(눈?집:놀이동산)
+});
+console.log(preNewData);
+
+// 2. 화면 대상에 태그 만들어 넣기
+// 대상선정 : .preview-box>div
+const preBox = dFn.qsa(".preview-box>div");
+console.log(preBox);
+
+// 3. 대상을 순회하여 태그 넣기
+// 데이터 : 역순정렬을 한 미리보기 데이터넣기
+preBox.forEach((ele, idx) => {
+    ele.innerHTML = `
+    <div>
+        <h3>${preNewData[idx].title}</h3>
+        <p>${preNewData[idx].story}</p>
+    <div>
+    `;
+}); //////////// forEach ////////////////
