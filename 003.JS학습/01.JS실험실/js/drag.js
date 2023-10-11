e4319c0b5d6cc6dcd4d1bc73ef89a3132919cfa8
zdzd3643 <zdzd3643@gmail.com>
2023-10-11 17:59:26 +0900

    ff

--- 003.JS학습/01.JS실험실/js/drag.js
+++ 003.JS학습/01.JS실험실/js/drag.js
@@ -26,9 +26,12 @@ console.log(dFn);
     
     2. 프로세스
     (1) mousedown 이벤트에서는 시작위치값 저장
+    -> 모바일 이벤트 : touchstart 
     (2) mousemove 이벤트에서는 움직인위치와 시작위치 차이저장
+    -> 모바일 이벤트 : touchmove 
     (3) mousemove에서 차이값을 타겟요소의 left,top값에 반영
     (4) mouseup 이벤트에서는 다음 이동을 위한 마지막위치 저장
+    -> 모바일 이벤트 : touchend 
     (5) mousemove 이벤트에서 마지막위치로 부터의 이동을 계산함
     
     
@@ -39,7 +42,7 @@ console.log(dFn);
     const dtg = dFn.qsa(".dtg");
     // 2. 드래그 설정하기
     dtg.forEach(ele=>goDrag(ele)); 
-    
+
 /************************************************** 
     [ 드래그 다중적용 함수 만들기 ]
     함수명 : goDrag
@@ -62,7 +65,7 @@ function goDrag(ele) {
     // (5) 위치이동 차이 결과변수: result x, result y
     let rx, ry;
 
-    // 3. 함수만들기 /////////////////
+    // 2. 함수만들기 /////////////////
     // (1) 드래그 상태 true로 변경함수
     const dTrue = () => (drag = true);
 
@@ -70,31 +73,60 @@ function goDrag(ele) {
     const dFalse = () => (drag = false);
 
     // (3) 드래그 움직일때 작동함수
-    const dMove = () => {
+    const dMove = (e) => {
         console.log("드래그상태:", drag);
 
         // 드래그 상태일때만 실행
         if (drag) {
             // 1. 드래그 상태에서 움직일때 위치값 : mvx, mvy
-            mvx = event.pageX;
-            mvy = event.pageY;
+            // - pageX,pageY 는 일반브라우저용
+            // - touches[0].screenX, touches[0].screenY는 터치스크린용
+            mvx = e.pageX || e.touches[0].screenX;
+            mvy = e.pageY || e.touches[0].screenY;
+
+            // 2. 움직일때 위치값 - 처음위치값 : rx, ry
+            // x축값은 left값, y축값은 top값 이동이다!
+            rx = mvx - fx;
+            ry = mvy - fy;
+            // 순수하게 움직인 거리를 계산! -> 가장중요한 핵심!!!
+
+            // 3. x,y 움직인 위치값을 타겟요소에 적용!
+            // 대상: 전달된 드래그 요소 -> ele변수
+            ele.style.left = rx+lx+'px';
+            ele.style.top = ry+ly+'px';
+            // 한번 드래그 후 다시 드래그시 움직인 위치값이 필요함!
+            // -> 마지막 위치값 저장필요! -> lx, ly
+            // -> 항상 최종위치에서 움직인 위치를 더한다!!!
+
+            // 4. z-index값을 모두 초기화후 드래그 대상만 높여줌!
+            dtg.forEach(ele=>ele.style.zIndex=0);
+            ele.style.zIndex = 1;
 
             // 값확인
+            console.log(`fx:${fx} | fy:${fy}`);
             console.log(`mvx:${mvx} | mvy:${mvy}`);
+            console.log(`rx:${rx} | ry:${ry}`);
+            console.log(`lx:${lx} | ly:${ly}`);
         } ////////////// if //////////////
+
+        // 커서 편손(grab)/쥔손(grabbing) 상태 변경하기
+        ele.style.cursor = drag?'grabbing':'grab';
+        
     }; ////////////// dMove 함수 ////////////
 
     // (4) 첫번째 위치포인트 셋팅함수 : fx, fy
     const firstPoint = () => {
-        fx = event.pageX;
-        fy = event.pageY;
+        fx = event.pageX || event.touches[0].screenX;
+        fy = event.pageY || event.touches[0].screenY;
+        // console.log('첫포인트:',fx,'|',fy);
     }; ///////////// firstPoint함수 /////////////
-
+    
     // (5) 마지막 위치포인트 셋팅함수 : lx, ly
     const lastPoint = () => {
         // 움직일때 위치값을 기존값에 계속 더함
         lx += rx;
         ly += ry;
+        // console.log('끝포인트:',lx,'|',ly);
     }; ///////////// lastPoint함수 //////////
 
     // 4. 이벤트 등록하기 /////////////////
@@ -104,15 +136,33 @@ function goDrag(ele) {
         dTrue();
         firstPoint();
     }); ///////////// mousemove함수 ////////////
+
+    // 모바일 이벤트 추가 /////////
+    dFn.addEvt(ele,'touchstart',()=>{
+        dTrue();
+        firstPoint();
+    }); ///////////// touchstart함수 ////////////
+
     // (2) 마우스 올라갈때 : 드래그 false + 마지막 위치값 업데이트
     dFn.addEvt(ele,'mouseup',()=>{
         dFalse();
         lastPoint();
     }); ///////////// mousemove함수 ////////////
+
+    // 모바일 이벤트 추가 /////////
+    dFn.addEvt(ele,'touchend',()=>{
+        dTrue();
+        lastPoint();
+    }); ///////////// touchend함수 ////////////
+
     // (3) 마우스 움직일때 : 움직일 때 처리함수 호출
     dFn.addEvt(ele,'mousemove',dMove);
+
+    // 모바일 이벤트 추가 /////////
+    dFn.addEvt(ele,'touchmove',dMove);
+    ///////////// touchmove함수 ////////////
+
     // (4) 마우스 벗어날때 : 드래그 상태 false처리 함수 호출
     dFn.addEvt(ele,'mouseleave',dFalse);
-
-
+    
 } //////////////// goDrag 함수 ///////////////////
