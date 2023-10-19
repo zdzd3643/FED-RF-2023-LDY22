80f0fce26f03e9d57bef22eed496fd97896b342f
zdzd3643 <zdzd3643@gmail.com>
2023-10-19 18:10:04 +0900

    ff

---
+++ 004.React학습/react-ex/js/make_data.js
@@ -0,0 +1,25 @@
+// 데이터 유형에 맞게 만들기 ////////
+
+let aa = ["man","woman","kids"];
+let gname = ["반스캐쥬얼","반스어얼리","반스뉴진스","반스스타일"]
+let gprice = ["43000","55000","76000","48000","66000"];
+
+// 호출시 : rdm[아이템개수]
+let rdm = (x)=> Math.floor(Math.random()*x)
+
+let bb = '';
+
+for(let x=1;x<19;x++){
+bb += `
+    {
+        "idx":'${x}',
+        "gname":'${gname[rdm[4]]}',
+        "category":'${category[rdm[3]]}',
+        "gprice":'${gprice[rdm[5]]}'
+
+    },
+`;
+}
+console.log(bb)
+
+
