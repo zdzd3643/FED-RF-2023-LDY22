3f061af6b1b8c45c5c75b661a0d01e66a9bfadd2
zdzd3643 <zdzd3643@gmail.com>
2023-10-30 10:43:52 +0900

    ff

---
+++ 005.jQuery학습/002.실전연습/05_DragGallery/js/icecream.js
@@ -0,0 +1,44 @@
+// 아이스크림 갤러리 JS - icecream.js
+
+$(()=>{ ///////////// jQB /////////////
+    // 요구사항: 아이스크림 갤러리를 드래그하여
+    // 이동시키며 양끝에 이동한계를 설정한다!
+
+    // 트랜지션 시간설정변수
+    const TRS_TIME_DT = '.8s ease-out';
+    const TRS_TIME_MOB = '.3s ease-out';
+    // 이징에 in이 들어가면 처음에 답답함!
+
+    // 1. 대상선정 : #move
+    // - 아이스크림 리스트를 모두 담고 있는 박스
+    const target = $('#move');
+
+    // 2. 변경내용 : 제이쿼리 UI 드래그 설정하기
+    target.draggable({
+        axis:'x' // x축고정
+    })
+    // 2-2. 타겟에 트랜지션 설정
+    .css({
+        transition: TRS_TIME_DT
+    }); /////////// css /////////////
+
+    // 2-3. 한계값 설정하기 ///////////
+    
+    // 2-3-1. 화면크기 업데이트 함수
+    const updataWin = () => $(window).width();
+    // 2-3-2. 최초 윈도우가로크기 업데이트
+    let winW = updataWin();
+    // 2-3-3. 윈도우 리사이즈시 윈도우가로크기 업데이트
+    $(window).resize(()=>{
+        winW = updataWin();
+        // console.log('업데이트 화면가로크기:',winW);
+    }); /////////// resize ////////////
+
+    
+    // console.log('화면가로크기:',winW);
+
+    // 2-3-4. 첫번째 한계값 설정하기 : 화면크기의 1/3로 설정
+    let firstPoint = winW / 3;
+    console.log('첫번째 한계값:',firstPoint);
+
+}); //////////////// jQB ///////////////
