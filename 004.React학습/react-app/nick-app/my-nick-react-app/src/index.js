3c83e943a3b312a8f9fb988f387186d2954c7367
zdzd3643 <zdzd3643@gmail.com>
2023-11-09 14:39:11 +0900

    ff

--- 004.React학습/react-app/nick-app/my-nick-react-app/src/index.js
+++ 004.React학습/react-app/nick-app/my-nick-react-app/src/index.js
@@ -1,17 +1,20 @@
+// index.js는 public/index.html 페이지에 적용되는 컴포넌트다!->루트 컴포넌트
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { TopArea } from './dc/TopArea';
+// css 도 불러온다!
+import './css/index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App(){
+  return(
+    <TopArea />
+  )
+}
+
+
+// 컴포넌트 출력 //////////
+// 먼저 root객체만들고
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+// render메서드로 출력
+root.render(<App />);
