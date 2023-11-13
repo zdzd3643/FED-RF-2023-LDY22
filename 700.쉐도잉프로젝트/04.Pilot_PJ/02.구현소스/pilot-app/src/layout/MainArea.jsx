974a683542d0df22995653c645a3f48e93ab7512
zdzd3643 <zdzd3643@gmail.com>
2023-11-13 16:30:02 +0900

    ff

---
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/layout/MainArea.jsx
@@ -0,0 +1,20 @@
+// Pilot PJ 메인영역 공통 컴포넌트 
+
+import { MainCont } from "../page/MainCont";
+import { MenSub } from "../page/MenSub";
+import { WomenSub } from "../page/WomenSub";
+import { StyleSub } from "../page/StyleSub";
+
+export function MainArea(props){
+    // props.
+    return(
+      <>
+        <h1>메인영역</h1>
+        {props.page=='main'&&<MainCont />}
+        {props.page=='men'&&<MenSub />}
+        {props.page=='women'&&<WomenSub />}
+        {props.page=='style'&&<StyleSub />}
+
+      </>
+    );
+} ///////////////////// MainArea 컴포넌트 //////////////////
