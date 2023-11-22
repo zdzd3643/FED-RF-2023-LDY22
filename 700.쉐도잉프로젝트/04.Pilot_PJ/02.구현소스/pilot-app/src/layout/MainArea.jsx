06f55050546889b629a623cc686f6f93604818b1
zdzd3643 <zdzd3643@gmail.com>
2023-11-22 13:15:01 +0900

    ff

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/layout/MainArea.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/layout/MainArea.jsx
@@ -1,18 +1,18 @@
-// Pilot PJ 메인영역 공통 컴포넌트 
+// Pilot PJ 메인영역 공통 컴포넌트
 
-import { MainCont } from "../page/MainCont";
-import { MenSub } from "../page/MenSub";
-import { WomenSub } from "../page/WomenSub";
-import { StyleSub } from "../page/StyleSub";
+import { Fashion } from "../pages/Fashion";
+import { MainCont } from "../pages/MainCont";
 
+// 라우터 역할을 하는 MainArea 컴포넌트 ////////
 export function MainArea(props){
     // props.page 속성값으로 main/men/women/style
     return(
-      <>
-        {props.page=='main'&&<MainCont />}
-        {props.page=='men'&&<MenSub />}
-        {props.page=='women'&&<WomenSub />}
-        {props.page=='style'&&<StyleSub />}
-      </>
+        <>
+            {
+                // main이 아니면 서브 Fashion이동
+                props.page=='main'?<MainCont />:
+                <Fashion cat={props.page} />
+            }
+        </>
     );
-} ///////////////////// MainArea 컴포넌트 //////////////////
+} //////////////// MainArea 컴포넌트 //////////
---
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/PilotContext.jsx
@@ -0,0 +1,5 @@
+// Pilot PJ 컨텍스트 API 설정
+
+import { createContext } from "react";
+
+export const pCon = createContext();
