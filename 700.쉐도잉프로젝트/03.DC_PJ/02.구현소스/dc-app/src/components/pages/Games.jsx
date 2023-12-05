cd42a97f02c0152415552eb449dacd22a64698ab
zdzd3643 <zdzd3643@gmail.com>
2023-12-05 20:24:11 +0900

    ff

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Games.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Games.jsx
@@ -1,7 +1,7 @@
 // 게임페이지 메인컨텐츠
 
 import { Banner } from "../modules/Banner";
-import { VidIntro } from "../modules/Vidintro";
+import { VidIntro } from "../modules/VidIntro";
 
 export function Games(){
     return(
